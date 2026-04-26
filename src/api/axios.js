3f91014d6a0cee4123bee3eaa06e517caa392813
Abhishekmt18 <abhiab3516@gmail.com>
2026-04-27 00:10:21 +0530

    added env config for deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // ✅ REQUEST INTERCEPTOR (attach token)
@@ -34,7 +34,7 @@ api.interceptors.response.use(
         const refreshToken = localStorage.getItem("refreshToken");
 
         const res = await axios.post(
-          "http://localhost:8080/api/users/refresh",
+          `${import.meta.env.VITE_API_URL}/api/users/refresh`,
           { refreshToken }
         );
 
